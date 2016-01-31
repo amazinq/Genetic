39131256c1994f17876dd028fa3dbe63f4576e67
amazinq <steffen.wissmann@hotmail.com>
2016-01-31 10:50:46 +0100

    Fixed keycollision - TODO: Fix fitness bug

--- Genetic/src/algorithm/Genetic_Labour.java
+++ Genetic/src/algorithm/Genetic_Labour.java
@@ -11,55 +11,57 @@ public class Genetic_Labour {
 	public static int MAXIMUM_X_POSITION = 1000;
 	public static int MAXIMUM_Y_POSITION = 1000;
 
-	private TreeMap<Double, Map> currentGeneration;
+	private ArrayList<Map> currentGeneration;
 	private Set<Double> keySet;
-	private double currentLowestFitness;
-	private double currentHighestFitness;
+	private Map currentLowestFitness;
+	private Map secondBestFitness;
+
 
 	public Genetic_Labour() {
-		currentGeneration = new TreeMap<Double, Map>();
-		currentLowestFitness = 1000;
-		currentHighestFitness = 0;
+		currentGeneration = new ArrayList<Map>();
+	}
+	
+	private void setFitnesses() {
+		for (Map map : currentGeneration) {
+			if(currentLowestFitness == null) {
+				currentLowestFitness = map;
+			} else if (secondBestFitness == null && map.getFitness() > currentLowestFitness.getFitness()) {
+				secondBestFitness = map;
+			} else if (secondBestFitness == null && map.getFitness() < currentLowestFitness.getFitness()) {
+				secondBestFitness = currentLowestFitness;
+				currentLowestFitness = map;
+			} else if (map.getFitness() < currentLowestFitness.getFitness()) {
+				secondBestFitness = currentLowestFitness;
+				currentLowestFitness = map;
+			} else if (map.getFitness() > currentLowestFitness.getFitness() && map.getFitness() < secondBestFitness.getFitness()) {
+				secondBestFitness = map;
+			}
+		}
 	}
 
 	public Map generateMaps() {
 		for (int i = 0; i < 8; i++) {
 			Map currMap = new Map();
 			currMap.generateMap();
-			currentGeneration.put(currMap.getFitness(), currMap);
+			currentGeneration.add(currMap);
 		}
-		keySet = currentGeneration.keySet();
-		for (Double key : keySet) {
-			Map map = currentGeneration.get(key);
-			if (map.getFitness() < currentLowestFitness) {
-				currentLowestFitness = map.getFitness();
-			} else if (map.getFitness() > currentHighestFitness) {
-				currentHighestFitness = map.getFitness();
-			}
-		}
+		setFitnesses();
 	
-		System.out.println(currentLowestFitness);
-		while (currentLowestFitness > 10) {
+		System.out.println(currentLowestFitness.getFitness());
+		int xyz = 0;
+		long time = System.currentTimeMillis();
+		while (currentLowestFitness.getFitness() > 10) {
 //			System.out.println(currentGeneration.size());
-			Map bestFitness = currentGeneration.get(currentLowestFitness);
 			// größe größer 2
-			Map secondBestFitness = null;
 			int iterator = 0;
-			for (double key : keySet) {
-				if (iterator == 1) {
-					secondBestFitness = currentGeneration.get(key);
-					break;
-				}
-				iterator++;
-			}
-			int crossDivider = new Random().nextInt(bestFitness.getCellList()
+			int crossDivider = new Random().nextInt(currentLowestFitness.getCellList()
 					.size());
 			Map childMap = new Map();
 			ArrayList<Cell> childCellList = new ArrayList<Cell>();
 			Random random = new Random();
 			for (int i = 0; i < crossDivider; i++) {
 				if (random.nextInt(100) >= 2) {
-					childCellList.add(bestFitness.getCellList().get(i));
+					childCellList.add(currentLowestFitness.getCellList().get(i));
 				} else {
 					childCellList.add(new Cell(new Point(random.nextInt(MAXIMUM_X_POSITION), random.nextInt(MAXIMUM_Y_POSITION))));
 				}
@@ -74,19 +76,13 @@ public class Genetic_Labour {
 			childMap.setCellList(childCellList);
 //			System.out.printf("Elems: %s\n", currentGeneration.keySet().stream().map(Ströing::valueOf).reduce((a, b) -> String.format("%s %s", a, b)).get());
 //			System.out.printf("Fitness: %f\n", childMap.getFitness());
-			currentGeneration.remove(secondBestFitness.getFitness());
-			currentGeneration.put(childMap.getFitness(), childMap);
-			keySet = currentGeneration.keySet();
-			for (double key : keySet) {
-				Map map = currentGeneration.get(key);
-				if (map.getFitness() < currentLowestFitness) {
-					currentLowestFitness = map.getFitness();
-				} else if (map.getFitness() > currentHighestFitness) {
-					currentHighestFitness = map.getFitness();
-				}
-			}
+			currentGeneration.remove(secondBestFitness);
+			currentGeneration.add(childMap);
+			setFitnesses();
+			//System.out.println(currentLowestFitness.getFitness());
+			xyz++;
 		}
-		System.out.println(currentLowestFitness);
-		return currentGeneration.get(currentGeneration.firstKey());
+		System.out.println("Number of Generations: " + xyz + " Time needed: " + (System.currentTimeMillis() - time));
+		return currentLowestFitness;
 	}
 }
